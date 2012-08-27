4cf63758ea7ed9e5a026b8d75acea17dca8f8c51
David Kim <david.kim@enthought.com>
2012-08-27 11:48:25 -0400

    Disqus setting added

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,19 +3,21 @@
 
 AUTHOR = u"David Kim"
 SITENAME = u"Babelscedastic"
-SITEURL = ''
+SITEURL = 'http://davidjk.github.com/babelscedastic/'
 
 #TIMEZONE = 'Europe/Paris'
 OUTPUT_PATH ='/Users/davidkim/Documents/git_repos/pelican_output/babelscedastic/'
-
-
+DEFAULT_CATEGORY = 'Articles'
+DISQUS_SITENAME = 'babelscedastic'
 DEFAULT_LANG = 'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+LINKS =  (('Enthought', 'http://www.enthought.com'),
+		  ('Numpy/Scipy', 'http://www.scipy.org'),
+		  ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
           ('Jinja2', 'http://jinja.pocoo.org'),
-          ('Enthought', 'http://www.enthought.com'),)
+          )
 
 # Social widget
 #SOCIAL = (('You can add links in your config file', '#'),
